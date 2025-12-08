49587f91384b6b92bc66b40ea4a70b84fb2d4888
falck5561-ux <falck5561@gmail.com>
2025-12-08 10:37:52 -0700

    Configuracion lista

--- src/pages/ClientePage.jsx
+++ src/pages/ClientePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import toast from 'react-hot-toast';
 import { motion } from 'framer-motion';
 import { loadStripe } from '@stripe/stripe-js';
